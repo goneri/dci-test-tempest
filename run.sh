be7ac6e6e1472332213aa6744fc4f9f474347053
Gonéri Le Bouder <goneri@redhat.com>
2016-05-20 17:09:44 -0400

    initial import

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir tempest
+(
+    source overcloudrc
+    cd tempest
+    /usr/share/openstack-tempest-liberty/tools/configure-tempest-directory
+    tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
+    # tools/run-tests.sh .*smoke
+    tools/run-tests.sh
+)
+ls -l tempest/tempest.xml tempest/tempest.log
