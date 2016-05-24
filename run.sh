61102694413d88d7c55fcb1295e584eedc7efc95
Gonéri Le Bouder <goneri@redhat.com>
2016-05-24 14:25:43 -0400

    call testr directely

--- run.sh
+++ run.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 sudo yum install -y openstack-tempest-liberty python-tempest-lib
 mkdir tempest
-(
-    source overcloudrc
-    cd tempest
-    /usr/share/openstack-tempest-liberty/tools/configure-tempest-directory
-    tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
-    # tools/run-tests.sh .*smoke
-    tools/run-tests.sh
-)
-ls -l tempest/tempest.xml tempest/tempest.log
-cp tempest/tempest.xml result.xml
+
+source overcloudrc
+cd tempest
+/usr/share/openstack-tempest-liberty/tools/configure-tempest-directory
+tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
+
+testr run | tee >( subunit2junitxml --output-to=result.xml ) | subunit-trace --no-failure-debug -f
