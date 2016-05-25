e40c9326e746e5f890a6c43c62443cd1a360b076
Gonéri Le Bouder <goneri@redhat.com>
2016-05-25 14:59:54 -0400

    idempotence of the script

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -eux
 sudo yum install -y openstack-tempest-liberty python-tempest-lib
-mkdir tempest
+
+[ -d tempest ] || mkdir tempest
 
 source overcloudrc
 cd tempest
 /usr/share/openstack-tempest-liberty/tools/configure-tempest-directory
 tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
 
-testr init
-testr run | tee >( subunit2junitxml --output-to=result.xml ) | subunit-trace --no-failure-debug -f
+[ -d .testrepository ] || testr init
+testr run --subunit |  subunit2junitxml --output-to=../result.xml
