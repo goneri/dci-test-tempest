c97f84cd8ca2ed346ad5e0812c779db43ebef8d0
Gonéri Le Bouder <goneri@redhat.com>
2016-05-26 13:36:32 -0400

    ignore testr error and run with it with --parallel

--- run.sh
+++ run.sh
@@ -10,4 +10,5 @@ cd tempest
 tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
 
 [ -d .testrepository ] || testr init
-testr run --subunit |  subunit2junitxml --output-to=../result.xml
+set +e
+testr run --subunit --parallel|subunit2junitxml --output-to=../result.xml
