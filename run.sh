1c127966697535b5a05aa75be0a2edcbeaf30d9c
Gonéri Le Bouder <goneri@redhat.com>
2016-06-17 16:33:43 -0400

    pass a parameter with the stack name

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eux
+stack_name=$1
 sudo yum install -y openstack-tempest-liberty python-tempest-lib
 
 # do not test manila
@@ -7,7 +8,7 @@ sudo yum remove -y openstack-rally python-manila
 
 [ -d tempest ] || mkdir tempest
 
-source overcloudrc
+source ${stack_name}rc
 cd tempest
 /usr/share/openstack-tempest-liberty/tools/configure-tempest-directory
 tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
