17b960983bb3d1341637075a3b51e5bfe44a8200
Gonéri Le Bouder <goneri@redhat.com>
2016-07-20 23:43:43 +0200

    add support for OSP9

--- run.sh
+++ run.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 stack_name=$1
-sudo yum install -y openstack-tempest-liberty python-tempest-lib
 
-# do not test manila
-sudo yum remove -y openstack-rally python-manila
+osp_version=$(yum info rhosp-director-images 2>&1|awk '/Version/ {print $3}')
+case $osp_version in
+    '8.0')
+    sudo yum install -y openstack-tempest-liberty python-tempest-lib
+    # do not test manila
+    sudo yum remove -y openstack-rally python-manila
+    tempest_dir='/usr/share/openstack-tempest-liberty'
+    ;;
+    '9.0')
+    sudo yum install -y openstack-tempest
+    tempest_dir='/usr/share/openstack-tempest-10.0.0'
+    ;;
+esac
 
 [ -d tempest ] || mkdir tempest
 
 source ${stack_name}rc
 cd tempest
-/usr/share/openstack-tempest-liberty/tools/configure-tempest-directory
+
+${tempest_dir}/tools/configure-tempest-directory
 tools/config_tempest.py --deployer-input ~/tempest-deployer-input.conf --debug --create identity.uri $OS_AUTH_URL identity.admin_password $OS_PASSWORD object-storage-feature-enabled.discoverability False
 
 [ -d .testrepository ] || testr init
